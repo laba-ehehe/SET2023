e72e000497a0581c4a1bbdb10ac9926f9964d175
Mannvika <mannyjainlakes@gmail.com>
2024-01-11 21:43:36 -0500

    Removed schedule and global variables; Merged everything one function; Added arguments for better functionality

--- periodic_image_capture.py
+++ periodic_image_capture.py
@@ -3,62 +3,74 @@
 # Periodic Image Capture
 
 import cv2
-import schedule
+import os
+import time
 
-# Number or address for the USB Cam
-cameraNumber = 0
+def periodic_image_capture(file_path: str, clears_images: bool, cameraNumber, seconds_between_capture):
+    # Get USB camera
+    camera = cv2.VideoCapture(cameraNumber)
 
-# Number of seconds between image captures
-seconds_between_capture = 10
+    # Start the num of images at 0
+    num_of_images = 0
 
-# Get USB camera
-camera = cv2.VideoCapture(cameraNumber)
+    # If we do not clear the images before starting the function
+    if clears_images == False:
+        # Go through each file in the images directory and add to the image counter
+        for path in os.listdir(file_path):
+            if os.path.isfile(os.path.join(file_path, path)):
+                num_of_images += 1; 
+        
+    # Image file extension
+    file_extension = "png"
 
-# Number of current images
-num_of_images = 0
+    # Initalize the previous time at 0
+    previous_time = 0
 
-# Image file extension
-file_extension = "png"
+    if camera.isOpened:
+        print("WORKING")
+        while True:
+            # Capture the time this loop started
+            inital_time = time.time()
 
-# Take a capture
-def capture():
-    global num_of_images
+            # Capture a frame of the camera
+            ret, frame = camera.read()
 
-    # Create an image name using the current image number and file extension
-    image_name = "image_{}.".format(num_of_images) + file_extension
+            # Display the Frame
+            cv2.imshow('frame', frame)
 
-    # Write the image to a file
-    cv2.imwrite(image_name, frame)
+            '''THIS WORKS BUT WILL PAUSE THE ENTIRE PREVIEW DISPLAY'''
+            '''WE CAN IMPLEMENT EITHER METHOD DEPENDING ON WHAT WE NEED'''
+            #time.sleep(seconds_between_capture)
 
-    print("Image Taken:", num_of_images)
+            # Check if enough time has passed between the previous capture and the current iteration of the loop
+            if inital_time - previous_time >= seconds_between_capture or previous_time == 0:
+                # Create an image name using the current image number and file extension
+                image_name = "image_{}.".format(num_of_images) + file_extension
 
-    # Add one to the number of images
-    num_of_images += 1
+                # Write the image to the correct filepath
+                cv2.imwrite(file_path + image_name, frame)
 
-# Schedules a capture to be called at each interval
-schedule.every(seconds_between_capture).seconds.do(capture)
+                print("Image Taken:", num_of_images)
 
-if camera.isOpened:
-    print("WORKING")
+                # Add one to the number of images
+                num_of_images += 1
 
-    while True:
-        # Capture a frame of the camera
-        ret, frame = camera.read()
+                # Set the previous_time to the current time
+                previous_time = time.time()
 
-        # Display the Frame
-        cv2.imshow('frame', frame)
+            # Close the application if q is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                print("Closing")
+                break
+    else:
+        print("FAILED")
 
-        # Run the next pending capture call
-        schedule.run_pending()
-
-        # Close the application if q is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            print("Closing application")
-            break
-else:
-    print("FAILED")
+if __name__ == "__main__":
+    '''Starts Image Counter at 0: '''
+    periodic_image_capture("../", True, 0, 5)
+    '''Starts Image Counter at how many ever images we already have: '''
+    #periodic_image_capture("../", False, 0, 5)
     
-
 # To Do Connect to ROS Publisher
 
 
